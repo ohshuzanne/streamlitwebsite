5f8bb9885222a73e7eb55d282543402357244dcf
ohshuzanne <suzannelts02@gmail.com>
2024-05-26 16:07:10 +0800

    improved css styling for website

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ from streamlit_extras.let_it_rain import rain
 #set up the page's configurations like sidebars and stuff
 #emoji website is on webfx emoji cheat sheets or sumn like that
 #default layout is centered, but wide makes it take up the whole screen width
-st.set_page_config(page_title="Shu's Website", page_icon = ":cookie:", layout="wide", )
+st.set_page_config(page_title="Shu's Website", page_icon = ":cookie:", layout="wide",)
 
+#default
+soc_med_links= {
+    "LinkedIn": "https://www.linkedin.com/in/suzanne-lai-tzi-syuen-034103254/",
+    "GitHub": "https://github.com/ohshuzanne",
+}
 
 #functions
 def lottie_loader(url):
@@ -27,6 +32,7 @@ def my_css(file):
 
 my_css("style/style.css")
 
+
 #assets
 lottie_polyfox = "https://lottie.host/f8f6abbe-7797-48cb-9d72-e3f006de0987/kzrdAgrilI.json"
 img_savvy = Image.open("image_assets/1.png")
@@ -34,17 +40,20 @@ img_kitajom = Image.open("image_assets/KitaJombanner.png")
 
 run_snow_animation()
 
+
 #header
 with st.container():
     st.subheader("My name is Shu :baby_chick:")
-    st.title("Badminton fan first, AI engineer second")
+    st.markdown('<h1 class="gradient-title">Badminton fan first, AI engineer second</h1>', unsafe_allow_html=True)
     st.write("I'm currently a second-year student finding the joy in using Python (the only language I'm good at) (jk) to optimize every aspect of my life")
-    st.write("[Learn More >](https://www.linkedin.com/in/suzanne-lai-tzi-syuen-034103254/)")
+    socmed_html = '<div class="socmed-links">' + ' '.join([f'<a href="{link}">{platform}</a>' for platform, link in soc_med_links.items()]) + '</div>'
+    st.markdown(socmed_html, unsafe_allow_html=True)
+
 
 #about me
 with st.container():
     st.write("---")
-    left_section, right_section = st.columns(2)
+    left_section, right_section = st.columns(2, gap="medium")
     with left_section:
         st.header("About me")
         st.write("""
@@ -57,36 +66,38 @@ with st.container():
                  
                  """)
     with right_section:
-        st_lottie(lottie_polyfox, height = 200, key="polyfox")
+        st_lottie(lottie_polyfox, height = 250, key="polyfox")
 
 #projects
 with st.container():
     st.write("---")
     st.header("My Projects")
-    st.write("###")
-    image_section, text_section = st.columns((1,2))
+    image_section, text_section = st.columns((1,2), gap="large")
     with image_section:
         st.image(img_savvy)
 
     with text_section:
         st.subheader("Savvy: A debt management application")
         st.write("""
-                 Visit the Github repo for this award-winning Flutter app that helps users manage their
-                 debts and finances through gamified learning.
+                 - First place in Varsity Hackathon 2024
+                 - Gamifies debt repayment and financial education
+                 - Motivates users through virtual "Savvy Pets"
+                 - Integrates Gemini for in-app chat feature, AI categorization and summarization
+                 - FlutterFire and Gemini
                  """)
         st.markdown("[Click me!](https://github.com/ohshuzanne/savvy)")
 with st.container():
-    image_section, text_section = st.columns((1,2))
+    image_section, text_section = st.columns((1,2), gap="large")
     with image_section:
         st.image(img_kitajom)
 
     with text_section:
         st.subheader("KitaJom: A sustainable travelling app for vendors")
         st.write("""
-                 Created independently as a school assignment, this is a sustainable travelling
-                 application developed using Flutter. Through user-friendly functionalities,
-                 vendors can manage their listings, bookings, and user reviews, all while
-                 promoting responsible travel practices within the country.
+                 - Individual university assignment (4.0)
+                 - Sustainable travelling application for vendors to manage their listings, bookings, user reviews and so on
+                 - Created to promote responsible travel practices within the country
+                 - FlutterFire
 
                  """)
         st.markdown("[Click me!](https://github.com/ohshuzanne/kitajom_vendor)")
